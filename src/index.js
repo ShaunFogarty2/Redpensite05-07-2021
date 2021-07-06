f58977c4d2b753a59f5a93d8ba9c9825b0aea099
ShaunFogarty2 <77686637+ShaunFogarty2@users.noreply.github.com>
2021-07-06 01:17:38 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
 <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="dm-supports dm-passiveeventlisteners pointer skrollr skrollr-desktop"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
