55f199d49152f90e8e5803297b07d607b72178e6
ShaunFogarty2 <77686637+ShaunFogarty2@users.noreply.github.com>
2021-07-07 16:14:44 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { Navigation, Footer, Home, About, Contact } from "./components";
+import { Home } from "./redpen site";
 function App() {
   return (
     <div className="App">
@@ -8,8 +8,6 @@ function App() {
         <Navigation />
         <Switch>
           <Route path="/" exact component={() => <Home />} />
-          <Route path="/about" exact component={() => <About />} />
-          <Route path="/contact" exact component={() => <Contact />} />
         </Switch>
         <Footer />
       </Router>
