2a3dab474e86d93cbf8f0deac5fbce8c2bab5a31
ShaunFogarty2 <77686637+ShaunFogarty2@users.noreply.github.com>
2021-07-07 16:59:53 +0100

    Update Home.jsx

--- src/redpen site/Home.jsx
+++ src/redpen site/Home.jsx
@@ -1,4 +1,8 @@
-<!DOCTYPE html>
+import React from "react";
+
+function Home() {
+  return (
+	  <!DOCTYPE html>
 <html xmlns="http://www.w3.org/1999/xhtml" lang="en" class="dm-supports dm-passiveeventlisteners pointer skrollr skrollr-desktop"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
     
 
@@ -1846,3 +1850,8 @@ setSmartSiteCookies();
 
 
 </body></html>
+
+  );
+}
+
+export default Home;
