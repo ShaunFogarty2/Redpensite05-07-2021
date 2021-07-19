6cc672204bfb17c2c7ef8c13499a8e0b81ecc717
ShaunFogarty2 <77686637+ShaunFogarty2@users.noreply.github.com>
2021-07-19 14:17:44 +0100

    Add files via upload

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
-import React from 'react';
-
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { Navigation, Footer, Home, About, Contact } from "./components";
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  return (
+    <div className="App">
+      <Router>
+        <Navigation />
+        <Switch>
+          <Route path="/" exact component={() => <Home />} />
+          <Route path="/about" exact component={() => <About />} />
+          <Route path="/contact" exact component={() => <Contact />} />
+        </Switch>
+        <Footer />
+      </Router>
+    </div>
+  );
 }
 
 export default App;
